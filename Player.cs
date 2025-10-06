1916fb19cea55b595f80357befe0edfe53be3df7
suhun7958D <suhun7958@gmail.com>
2025-10-06 17:48:44 +0900

    Day 1 : Initial commit
    선수 클래스, 선수 기록 클래스

---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGameProject
+{
+    public class Player
+    {
+        public string Name { get; private set; } 
+        public int Hitting { get; private set; } 
+        public int Power { get; private set; }   
+        public int Eye { get; private set; }     
+        public int Speed { get; private set; }   
+        public int Pitching { get; private set; } 
+        public string Position { get; set; }      
+        public PlayerStats Stats { get; private set; } 
+
+        
+        public bool IsPitcher { get { return Position == "P"; } } // 투수인지 판별
+
+
+        //생성자 : 선수 능력치와 포지션 초기화
+        public Player(string name, int hitting, int power, int eye, int speed, int pitching = 0, string position = "F") 
+        {
+            Name = name;
+            Hitting = Math.Max(0, Math.Min(100, hitting));            //능력치 제한
+            Power = Math.Max(0, Math.Min(100, power));                //능력치 제한
+            Eye = Math.Max(0, Math.Min(100, eye));                    //능력치 제한
+            Speed = Math.Max(0, Math.Min(100, speed));                //능력치 제한
+            Pitching = Math.Max(0, Math.Min(100, pitching));          //능력치 제한
+            Position = position;
+            Stats = new PlayerStats(); //경기 기록 초기화
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Position} H:{Hitting} P:{Power} E:{Eye} S:{Speed} Pitch:{Pitching})";
+        }
+    }
+}
---
+++ PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGameProject
+{
+    
+    //선수경기기록
+    // AB : 타수 H: 안타 HR: 홈런 RBI :타점 BB: 볼넷 SO: 삼진 R: 득점 AVG : 타율
+        
+    public class PlayerStats 
+    {
+        public int AB { get; set; }
+        public int H { get; set; }
+        public int HR { get; set; }
+        public int RBI { get; set; }
+        public int BB { get; set; }
+        public int SO { get; set; }
+        public int R { get; set; }
+
+        public override string ToString()
+        {
+            double avg = AB == 0 ? 0.0 : (double)H / AB;
+            return $"AB:{AB}, H:{H}, HR:{HR}, RBI:{RBI}, BB:{BB}, SO:{SO}, R:{R}, AVG:{avg:0.000}";
+        }
+    }
+}
